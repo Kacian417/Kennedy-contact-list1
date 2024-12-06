38b87f6b49d8f533b9327ecd1ac40d65fdbdc7f1
Kacian417 <knicho4492@gmail.com>
2024-12-06 02:14:53 +0000

    fixed appearance of layout

--- src/js/views/Contact.jsx
+++ src/js/views/Contact.jsx
@@ -9,9 +9,9 @@ const Contact = ({id, name, phone, email, address}) => {
     const {store, actions} = useContext(Context)
     return (
         <>
-            <img src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
-            <div>
-                <p className="fs-2 fw-bold">{name}</p>
+            <img src="https://xsgames.co/randomusers/avatar.php?g=pixel" className="col-3 rounded-circle m-2 d-flex align-items-center" height = "150px" width = "150px" alt="Contact Photo" />
+            <div className="col-7">
+                <p className="fs-2 fw-bold pe-5">{name}</p>
                 <p>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-telephone-fill" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
@@ -32,14 +32,14 @@ const Contact = ({id, name, phone, email, address}) => {
                     {address}
                 </p>
             </div>
-            <div>
+            <div className="col-1">
                 <Link to="/AddContact">
                 <svg 
                     xmlns="http://www.w3.org/2000/svg" 
                     width="25" 
                     height="25" 
                     fill="currentColor" 
-                    className="bi bi-pencil-fill" 
+                    className="bi bi-pencil-fill d-flex justify-content-end mt-2" 
                     viewBox="0 0 16 16"
                     onClick={() => {actions.updateContactData(contactData)}}>
                     <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
@@ -53,7 +53,7 @@ const Contact = ({id, name, phone, email, address}) => {
                     width="25" 
                     height="25" 
                     fill="currentColor" 
-                    className="bi bi-trash3-fill" 
+                    className="bi bi-trash3-fill d-flex justify-content-end mt-2 me-2" 
                     viewBox="0 0 16 16" 
                     onClick={() => {actions.deleteContact(id)}}>
                     <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
--- src/js/views/ContactList.jsx
+++ src/js/views/ContactList.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import Contact from "./Contact.jsx";
+import "../../styles/Contacts.css"
 
 import { Context } from "../store/appContext.js";
 
@@ -11,7 +12,7 @@ const ContactList = () => {
             <ul>
                 {store.contacts.map(contact => {
                     return(
-                        <li key={contact.id} className="contactListNames d-flex">
+                        <li key={contact.id} className="contactListNames d-flex border border-secondary">
                             <Contact 
                                 id={contact.id}
                                 name={contact.name}
